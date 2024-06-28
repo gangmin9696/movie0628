90f07cc8d63d95378edec1a270b9eff53552efff
gangmin9696 <poi2308@naver.com>
2024-06-28 15:20:38 +0900

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script setup>
-  import { ref , onMounted} from 'vue';
+  import { ref } from 'vue';
   import Card from './components/Card.vue'
 
   const movieList = ref([]);
@@ -51,9 +51,6 @@
   }
 
   
-  onMounted(()=>{
-    popular()
-  });
 
 </script>
 
